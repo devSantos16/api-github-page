5aa04ba498d979d5008fcca02a05bf921b5c45f3
devSantos16 <devsantos1999@gmail.com>
2022-07-29 20:58:18 -0300

    Create popup

--- api-git-hub/src/App.js
+++ api-git-hub/src/App.js
@@ -2,25 +2,28 @@ import React, { Component } from "react";
 import Container from "./assets/components/container"
 import ContainerItem from "./assets/components/container-item";
 import Section from "./assets/components/section";
-import ShowData from "./assets/js/script"
+import Popup from "./assets/components/popup";
+import generatePopup from "./assets/js/script"
 import imageGitHub from "./assets/img/logotipo-do-github.png"
 
 
-class App extends Component {
+ class App extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return (
+         return (
             <Section>
                 <Container>
                     <img className="img-container" src={imageGitHub} alt="Imagem do GitHub" />
                     <p>Api-GitHub</p>
                     <ContainerItem>
                         <input className="input-container" id="input-event" type="text" placeholder="Digite o nickname do github"></input>
-                        <button id="button-event" onClick={ShowData} className="button-container btn btn-success">Procurar</button>
+                        <button id="button-event" onClick={generatePopup} className="button-container btn btn-success">Procurar</button>
                     </ContainerItem>
                 </Container>
+                <Popup>
+                </Popup>
             </Section>
         )
     }
--- api-git-hub/src/assets/js/script.js
+++ api-git-hub/src/assets/js/script.js
@@ -3,6 +3,31 @@ import { token } from "./models/token";
 
 
 
+async function generatePopup(){
+    const object = await ShowData();
+    const popup = document.getElementsByClassName("popup")[0];
+    const image = document.getElementsByClassName("image-github")[0];
+    const name = document.getElementsByClassName("name-github")[0];
+    const username = document.getElementsByClassName("username-github")[0];
+    const button = document.createElement("a");
+
+
+    button.classList.add("github-button");
+    button.href = object.url;
+
+    button.ariaLabel = `Follow @${object.username} on GitHub`;
+    button.textContent = `Follow ${object.username}`;
+    document.getElementsByClassName("github-button-container")[0].appendChild(button);
+
+    image.src = object.image_github;
+    name.innerHTML = object.name;
+    username.innerHTML = object.username;
+
+    popup.classList.toggle("visibilityPopup");
+
+
+}
+
 async function ShowData(){
     const input = document.getElementById("input-event").value;
     const data = await ResponseUser(input);
@@ -11,7 +36,14 @@ async function ShowData(){
         window.alert(data.message);
     }
     else{
-        window.alert(data.login)
+        let objectData = {
+            username: data.login,
+            name: data.name,
+            url: data.html_url,
+            image_github: data.avatar_url
+            
+        }
+        return objectData;
     }
 }
 
@@ -27,4 +59,4 @@ async function ResponseUser(input) {
     }
 }
 
-export default ShowData
+export default generatePopup;
